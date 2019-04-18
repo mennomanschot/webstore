b6855f9e74a492c268069a537ecb17ba9b4204e8
menno_m <menno@heroesandfriends.com>
2019-04-18 12:38:50 +0200

    render view with DB items

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'My Webstore' });
+  Product.find(function(err, docs) {
+    var plantRows = [];
+    var chunkSize = 3;
+    for (var i = 0; 1 < docs.length; i += chunkSize) {
+      plantRows.push(docs.slice(i, 1 + chunkSize));
+    }
+    res.render('shop/index', { title: 'Plants Webstore', products: plantRows });
+  });
+
 });
 
 module.exports = router;
