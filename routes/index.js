2a733980049d1981bddbcf5c617849ef8886c8d0
menno_m <menno@heroesandfriends.com>
2019-04-18 14:38:00 +0200

    correct grid layout

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,25 @@ var Product = require('../models/product');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
-    var plantRows = [];
-    var chunkSize = 3;
-    for (var i = 0; 1 < docs.length; i += chunkSize) {
-      plantRows.push(docs.slice(i, 1 + chunkSize));
-    }
-    res.render('shop/index', { title: 'Plants Webstore', products: plantRows });
+    // var plantRows = [];
+    // var chunkSize = 3;
+    // for (var i = 0; 1 < docs.length; i += chunkSize) {
+    //   plantRows.push(docs.slice(i, i + chunkSize));
+    // }
+    res.render('shop/index', { title: 'Plants Webstore', products: docs });
   });
-
 });
 
 module.exports = router;
+//
+//
+//
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('shop/index', { title: 'My Webstore' });
+// });
+//
+// module.exports = router;
