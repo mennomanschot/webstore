5a3397a57d8746f03233a6d3ea4ba425b1e4bea2
menno_m <menno@heroesandfriends.com>
2019-10-04 16:33:59 +0200

    updated dependencies

--- config/passport.js
+++ config/passport.js
@@ -35,18 +35,19 @@ passport.use('local.signup', new localStrategy({
       }
       if (user) {
         return done(null, false, {message: 'email already in use'});
-      }
-      var newUser = new User();
-        newUser.email = email;
-        newUser.password = newUser.encryptPassword(password);
-        newUser.save(function(err, result) {
-            if (err) {
-              return done(err);
-            }
-            return done(null, newUser);
-        });
+        }
+        var newUser = new User();
+          newUser.email = email;
+          newUser.password = newUser.encryptPassword(password);
+          newUser.save(function(err, result) {
+              if (err) {
+                return done(err);
+              }
+              return done(null, newUser);
+          });
+      })
     })
-}));
+  );
 
 
 // Sign In Stretegy
@@ -55,26 +56,27 @@ passport.use('local.signin', new localStrategy({
   passwordField: 'password',
   passReqToCallback: true
 }, function(req, email, password, done) {
-  req.checkBody('email', 'Invalid email').notEmpty().isEmail();
-  req.checkBody('password', 'Invalid password').notEmpty();
-  var errors = req.validationErrors();
-  if (errors) {
-    var messages = [];
-    errors.forEach(function(error) {
-      messages.push(error.msg);
-    });
-    return done(null, false, req.flash('error', messages));
-  }
-  User.findOne({'email': email}, function(err, user){
-    if (err) {
-      return done(err);
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+      var messages = [];
+      errors.forEach(function(error) {
+        messages.push(error.msg);
+      });
+      return done(null, false, req.flash('error', messages));
     }
-    if (!user) {
-      return done(null, false, {message: 'No user found'});
-    }
-    if (!user.validPassword(password)) {
-      return done(null, false, {message: 'Wrong password'});
-    }
-    return done(null, user);
+    User.findOne({'email': email}, function(err, user){
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, {message: 'No user found'});
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, {message: 'Wrong password'});
+      }
+      return done(null, user);
+    })
   })
-}));
+);
